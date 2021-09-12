1ef5fed49037305149ada9864981133fd33e0501
Kira Oakley <kira@eight45.net>
2021-09-12 10:04:58 -0700

    Adds "duration" argument.

--- cli.js
+++ cli.js
@@ -1,12 +1,20 @@
 const baudio = require('baudio')
 const argv = require('minimist')(process.argv.slice(2),
-  { alias: { v: 'volume', f: 'freq' } })
+  { alias: { v: 'volume', f: 'freq', d: 'duration' } })
 
 const volume = argv.volume ? parseFloat(argv.volume) : 0.05
 const freq = argv.freq ? parseInt(argv.freq) : 262
+const duration = argv.duration ? parseInt(argv.duration) : Infinity
 
 const b = baudio(t => {
+  if (t >= duration) return 0
   return Math.sin(t * freq * Math.PI * 2) * volume
 })
 b.play()
 
+if (isFinite(duration)) {
+  setTimeout(() => {
+    console.log('done')
+    process.exit(0)
+  }, duration * 1000)
+}
